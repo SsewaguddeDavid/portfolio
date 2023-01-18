43f27ae56033b562db3071eced7424eed96058f2
Ssewagudde David <dsewagude10@gmail.com>
2023-01-18 23:09:49 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import david from '../assets/male_avatar.svg'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+export const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+        <div>
+            <img src={david} alt='logo' style={{width: '50px'}}/>
+        </div>
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+
+        <ul className= {!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>Work</li>
+            <li className='py-6 text-4xl'>Contact</li>
+        </ul>
+    </div>
+  )
+}
